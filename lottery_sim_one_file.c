8b649fadaa6c223aba649af54e25c35cdf9f7384
Liam Ward <44728707+liamward14@users.noreply.github.com>
2020-05-19 14:33:05 -0400

    Update to get continue response and avoid irregular input

--- lottery_sim_one_file.c
+++ lottery_sim_one_file.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include <time.h>
 
 //Global vars
@@ -7,21 +9,39 @@ int i;
 int drawn[6];
 int tracking[95];
 int select[6];
+int play=1;
 
 //Fxn prototypes
 void select_numbers(void);
 void display_winners(void);
 void generate_winners(void);
 void sort_winners(void);
+int check_win(void);
+int check_response(void);
 
 int main()
 {
     printf("Welcome to the Lottery Simulation!\n");
 
     //Execute simulation
-    generate_winners();
-    select_numbers();
-    display_winners();
+    while(play)
+    {
+        generate_winners();
+        sort_winners();
+        select_numbers();
+        display_winners();
+        int win_status = check_win();
+        if (win_status>=6)
+        {
+            printf("You won!\nThank you for playing!\n");
+        }
+        else
+        {
+            printf("\nYou lost, sorry\n");
+            play = check_response();
+        }
+    }
+
 
     return(0);
 }
@@ -44,6 +64,12 @@ void select_numbers(void)
         int selection;
         printf("Selection: ");
         scanf("%d",&selection);
+        while (selection<1||selection>51)
+        {
+            printf("\nInvalid input, please try again\n");
+            printf("Selection: ");
+            scanf("%d",&selection);
+        }
         select[i] = selection;
     }
 
@@ -71,12 +97,52 @@ void sort_winners(void)
     {
         for(y=0;y<6;y++)
         {
-            if (drawn[x]>drawn[y])
+            if (drawn[x]<drawn[y])
             {
                 int temp = drawn[y];
                 drawn[y] = drawn[x];
                 drawn[x] = temp;
             }
         }
+    }
+}
+
+int check_win(void)
+{
+    int x,y;
+    int sum=0;
+    for(x=0;x<6;x++)
+    {
+        for(y=0;y<6;y++)
+        {
+            if (drawn[x]==select[y])
+            {
+                sum++;
+            }
+        }
+    }
+    return(sum);
+}
+
+int check_response(void)
+{
+    char response;
+    printf("Would you like to play again?\n");
+    printf("Response ('y' or 'n'): ");
+    getchar();
+    scanf("%c",&response);
+    if (toupper(response)=='Y')
+    {
+        return(1);
+    }
+    else if (toupper(response)=='N')
+    {
+        printf("The game will exit now...\n");
+        return(0);
+    }
+    else
+    {
+        printf("Invalid response, please try again...\n");
+        check_response();
     }
 }
